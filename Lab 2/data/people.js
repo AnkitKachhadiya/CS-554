77220ff0c4545530151f4f6c17d4522ff07ea5bf
AnkitKachhadiya <28569675+AnkitKachhadiya@users.noreply.github.com>
2022-02-02 12:00:53 -0500

    tested redis in node

--- Lab 2/data/people.js
+++ Lab 2/data/people.js
@@ -1,6 +1,12 @@
 const peopleJson = require("./people.json");
 
-async function getById(_id) {}
+async function getById(_id) {
+    for (const currentPerson of peopleJson) {
+        if (currentPerson.id === parseInt(_id)) {
+            return currentPerson;
+        }
+    }
+}
 
 module.exports = {
     getById,
--- Lab 2/routes/api.js
+++ Lab 2/routes/api.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
+
+const validator = require("../helpers/validator");
+const ErrorCode = require("../helpers/error-code");
+
 const xss = require("xss");
+
 const redis = require("redis");
 const redisClient = redis.createClient();
 
@@ -12,14 +17,38 @@ const data = require("../data");
 const peopleData = data.people;
 
 router.get("/people/history", async (request, response) => {
-    console.log("history");
+    const history = await redisClient.get("history");
+
+    const accessHistory = history === null ? [] : JSON.parse(history);
+
+    response.json(accessHistory);
 });
 
 router.get("/people/:id", async (request, response) => {
-    await redisClient.set("demoKey", "demoValue");
-    const value = await redisClient.get("demoKey");
+    try {
+        const person = await peopleData.getById(request.params.id);
 
-    console.log(value);
+        const history = await redisClient.get("history");
+
+        if (history === null) {
+            await redisClient.set("history", JSON.stringify([person]));
+        } else {
+            const jsonHistory = JSON.parse(history);
+
+            jsonHistory.unshift(person);
+
+            await redisClient.set("history", JSON.stringify(jsonHistory));
+        }
+    } catch (error) {
+        console.log(error);
+        response.status(error.code || ErrorCode.INTERNAL_SERVER_ERROR).send({
+            serverResponse: error.message || "Error: Internal server error.",
+        });
+    }
 });
+
+const throwError = (code = 500, message = "Error: Internal Server Error") => {
+    throw { code, message };
+};
 
 module.exports = router;
