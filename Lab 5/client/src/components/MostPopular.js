b16600d2dfa37ae0fe065d4c17d96d155e8e4ae8
AnkitKachhadiya <28569675+AnkitKachhadiya@users.noreply.github.com>
2022-04-11 20:57:35 -0400

    mainstream

--- Lab 5/client/src/components/MostPopular.js
+++ Lab 5/client/src/components/MostPopular.js
@@ -2,9 +2,20 @@ import { useQuery } from "@apollo/client";
 import { GET_POPULAR_IMAGES } from "../queries";
 import { Loader } from "./styles/Loader.styled";
 import Cards from "./Cards";
+import { useState } from "react";
 
 function MostPopular() {
+    const [mainstreamTotal, setMainstreamTotal] = useState(0);
     const { data, loading } = useQuery(GET_POPULAR_IMAGES, {
+        onCompleted: (popularImages) => {
+            let total = 0;
+
+            popularImages.getTopTenBinnedPosts.map(
+                (currentImage) => (total += currentImage.numBinned)
+            );
+
+            setMainstreamTotal(total);
+        },
         fetchPolicy: "no-cache",
         notifyOnNetworkStatusChange: true,
     });
@@ -19,7 +30,14 @@ function MostPopular() {
                 {data &&
                 data.getTopTenBinnedPosts &&
                 data.getTopTenBinnedPosts.length > 0 ? (
-                    <Cards data={data.getTopTenBinnedPosts} mostPopular />
+                    <>
+                        <p className="text-center">
+                            {mainstreamTotal >= 200
+                                ? "Mainstream"
+                                : "Non-mainstream"}
+                        </p>
+                        <Cards data={data.getTopTenBinnedPosts} mostPopular />
+                    </>
                 ) : (
                     <p className="text-center">No Popular Images Found.</p>
                 )}
